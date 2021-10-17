4a738482583047e7bd7f88d1145fdf2bbc8303b9
javad pournosrat <pournosrat.javad@gmail.com>
2021-10-17 19:40:17 +0330

    create form with flask_wtf-DomainInput and methods

--- app.py
+++ app.py
@@ -2,12 +2,23 @@
 
 from flask import Flask
 from flask.templating import render_template
+from flask_wtf import FlaskForm
+from wtforms.fields.core import StringField
+from wtforms.fields.simple import SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
-@app.route('/')
+class DmainInput(FlaskForm):
+    Domain = StringField("Domain", validators=[DataRequired()])
+    Submit = SubmitField("submit")
+
+app.config['SECRET_KEY'] = 'mysuper secret key'
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    form = DmainInput()
+    return render_template('index.html', form=form)
 
 app.debug = True
 app.run()
