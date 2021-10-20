c0366a0aeb11a7df172ce926533d96f91b992408
javad pournosrat <pournosrat.javad@gmail.com>
2021-10-21 00:45:30 +0330

    add lat lon link_map

--- app.py
+++ app.py
@@ -32,6 +32,10 @@ def index():
     country = None
     city = None
     timezone = None
+    updated_date =None
+    map_link = None
+    lat = None
+    lon = None
 
     form = DmainInput()
     if form.validate_on_submit():
@@ -63,6 +67,14 @@ def index():
         country = jsn["country"]
         city = jsn["city"]
         timezone = jsn["timezone"]
+
+        lat = jsn["lat"]
+        lon = jsn["lon"]
+        map_link = f"https://www.latlong.net/c/?lat={lat}&long={lon}"
+
+        #updated_date
+        updated_date = whois.whois(str(Domain))["updated_date"]
+        updated_date = str(updated_date)
         
     return render_template('index.html',
     Domain = Domain,
@@ -74,8 +86,14 @@ def index():
     creation_date = creation_date,
     country = country,
     city = city,
-    timezone = timezone
+    timezone = timezone,
+    updated_date = updated_date,
+    map_link = map_link,
+    lat = lat,
+    lon = lon
     )
+
+#PORT SCANNER
 
 app.debug = True
 app.run()
